e22f0a30cf72c744c02d02fd9e0ee4c80fd4d490
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2023-06-25 22:13:00 -0400

    Add `isSafe` which can be modified for more specific use-cases :)

--- src/environment.js
+++ src/environment.js
@@ -48,10 +48,16 @@ module.exports = class Environment {
 		return this.parent.varExists(name);
 	}
 
+	isSafe(record, name) {
+		return record?.hasOwnProperty?.(name);
+	}
+
 	resolve(name, pos) {
-		if (this.record.hasOwnProperty(name)) return this;
+		// if (this.record.hasOwnProperty(name)) return this;
+		if (isSafe(this.record, name)) return this;
 		if (this.parent == null || !(this.parent instanceof Environment)) throw new ReferenceError(`Could not resolve variable '${name}' (${pos.filename}:${pos.line}:${pos.cursor})`);
-		if (this.parent.record?.hasOwnProperty?.(name)) return this.parent;
+		if (isSafe(this.parent.record, name)) return this.parent;
+		// if (this.parent.record?.hasOwnProperty?.(name)) return this.parent;
 
 		return this.parent.resolve(name, pos);
 	}
